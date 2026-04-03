0fa9e66d5cd8f14a08bf01d20f5be7d7e285224d
natetann <nathantan188@gmail.com>
2026-04-02 20:14:45 -0400

    feat: add welcome text and pyfiglet dependency

---
+++ jobtrackercli/main.py
@@ -0,0 +1,14 @@
+from rich.console import Console
+from rich.text import Text
+import pyfiglet
+
+console = Console()
+
+# Text definitions
+welcomeText = pyfiglet.figlet_format("JOB TRACKER CLI", font="standard", width=100).rstrip()
+coloredText = Text(welcomeText, style="bold yellow")
+authorText = Text("by: github.com/natetann", style="bold underline magenta")
+
+# Rich text prints
+console.print(coloredText, justify="center")
+console.print(authorText, justify="center")
